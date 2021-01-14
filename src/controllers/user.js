3f84e5acd6b566d84d90c2827ddd0fb9d4cb90e9
Максим Дорохов <plasticut@gmail.com>
2021-01-14 19:56:51 +0300

    Configure multer

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,6 +2,10 @@ module.exports = function(app) {
   const { getUser, getUserList, setAvatar } = require("../user.utils");
   var multer  = require('multer')
 
+  const { single } = multer({
+    dest: './uploads'
+  });
+
   app.get('/user/get-one', (req, res) => {
     getUser(req.query.id)
       .then(user => res.json(user.toJSON()));
@@ -12,7 +16,7 @@ module.exports = function(app) {
       .then(users => res.json(users));
   });
 
-  app.post('/user/set-avatar', multer.single(), (req, res) => {
+  app.post('/user/set-avatar', single(), (req, res) => {
     getUser(req.query.id)
       .then(user => {
         return setAvatar(user, req.files[0].path, req.query.storage)
